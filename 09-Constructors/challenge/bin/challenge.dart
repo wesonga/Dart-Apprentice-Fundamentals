aa7326f21826239c26247831d5ddce672be6e6db
Wesonga Bob <31077716+wesonga@users.noreply.github.com>
2024-05-21 01:30:48 +0300

    Challenge and exercise

--- 09-Constructors/challenge/bin/challenge.dart
+++ 09-Constructors/challenge/bin/challenge.dart
@@ -1,16 +1,33 @@
-class User {
-  int id = 0;
-  String name = '';
-
-  @override
-  String toString() {
-    return 'User(id: $id, name: $name)';
-  }
+void main() {
+  exercise1();
+  challenge1();
 }
 
-void main() {
-  final user = User();
-  user.id = 10;
-  user.name = 'Bob';
-  print(user);
+void challenge1() {
+  final student1 = Student('bert', 'bert', 95);
+  final student2 = Student('ernie', 'ernie', 85);
+  print(student1);
+  print(student2);
+}
+
+void exercise1() {
+  final number = PhoneNumber('555-555-555');
+  print(number);
+}
+
+class PhoneNumber {
+  const PhoneNumber(this.value);
+  final String value;
+}
+
+class Student {
+  Student(this.firstName, this.lastName, this.grade);
+
+  final firstName;
+  final lastName;
+  int grade = 0;
+
+  @override
+  String toString() => 'firstname: $firstName, lastname: $lastName, grade: $grade';
+  
 }
